dda6ad5f276e7121e09f63b6c2b150629a182eb4
David Payne <dpayne162@gmail.com>
2014-02-26 00:17:17 -0600

    change

--- README.md
+++
@@ -1,2 +0,0 @@
-davpayne.github.io
-==================
---
+++ Scripts/script.js
@@ -0,0 +1,33 @@
+var demoApp = angular.module('demoApp', []);
+
+
+demoApp.config(function ($routeprovider) {
+    $routeprovider
+        .when('/',
+        {
+            controller: 'simpleController',
+            templateUrl: 'Partials/View1.html'
+        })
+        .when('/view2',
+        {
+            controller: 'simpleController',
+            templateUrl: 'Partials/View2.html'
+        })
+        .otherwise({ redirectTo: '/' })
+});
+/*var controllers = {};
+controllers.simpleController = function($scope) {
+ $scope.customers = [{name:'Dave', city:'Denver'}, {name:'Napur', city:'Houston'}, {name:'Heedy', city:'Denver'}, {name:'Shriva', city:'Los Angeles'}];
+};
+demoApp.controller(controllers)*/
+demoApp.controller('simpleController', function($scope) {
+
+                $scope.customers = [{name:'Dave', city:'Denver'},
+                 {name:'Napur', city:'Houston'},
+                  {name:'Heedy', city:'Denver'}, 
+                  {name:'Shriva', city:'Los Angeles'}];
+
+                $scope.addCustomer = function() {
+                    $scope.customers.push({name: $scope.newCustomer.name, city: $scope.newCustomer.city})
+                }
+            });
