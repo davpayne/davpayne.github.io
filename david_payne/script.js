3fbdb034f7c2501d6b49818fb2cb388a9becf9c7
davpayne <dpayne162@gmail.com>
2016-01-31 12:44:31 -0500

    test

--- david_payne/script.js
+++ david_payne/script.js
@@ -1,6 +1,27 @@
 // Creates the main module and injects dependecies
 
 (function(){
-  var app = angular.module('proSite',[]);
+  var app = angular.module('proSite',['ngRoute']);
   app.controller('proSiteController',function(){});
+  app.config(function($routeProvider) {
+        $routeProvider
+
+            // route for the home page
+            .when('/', {
+                templateUrl : 'pages/home.html',
+                controller  : 'mainController'
+            })
+
+            // route for the about page
+            .when('/about', {
+                templateUrl : 'pages/about.html',
+                controller  : 'aboutController'
+            })
+
+            // route for the contact page
+            .when('/contact', {
+                templateUrl : 'pages/contact.html',
+                controller  : 'contactController'
+            });
+    });
 })();
